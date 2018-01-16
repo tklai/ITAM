6b846c9d1523026d91bcac71ce1bf7ab446d7f2c
Unknown <sakakilai@gmail.com>
2018-01-16 14:17:13 +0800

    [Dependencies] Add bootstrap-table

---
+++ resources/assets/js/bootstrap-table.js
@@ -0,0 +1,6 @@
+// Require main bootstrap-table
+require('bootstrap-table/src/bootstrap-table');
+
+// Require language files
+require('bootstrap-table/src/locale/bootstrap-table-en-US');
+require('bootstrap-table/src/locale/bootstrap-table-zh-TW');
