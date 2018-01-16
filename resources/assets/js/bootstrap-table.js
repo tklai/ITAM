b69b4aeea74ed0bc1aac5a99936e37f56b27ede3
Unknown <sakakilai@gmail.com>
2018-01-16 17:31:39 +0800

    [Dependencies] Add Open Iconic and cleanup
    Added Open Iconic as the icon set.
    Use minified code (prod) instead of dev for commiting.

--- resources/assets/js/bootstrap-table.js
+++ resources/assets/js/bootstrap-table.js
@@ -1,5 +1,6 @@
 // Require main bootstrap-table
 require('bootstrap-table/src/bootstrap-table');
+require('bootstrap-table/src/extensions/mobile/bootstrap-table-mobile')
 
 // Require language files
 require('bootstrap-table/src/locale/bootstrap-table-en-US');
