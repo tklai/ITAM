ff306b03662596a30d4953e30dbe007810120986
Unknown <sakakilai@gmail.com>
2018-01-16 11:31:51 +0800

    [UI] Redesign the login UI and landing page
    Previously, the app was using Bootstrap 3 as the UI Framework. Redesigned with Bootstrap 4 Beta.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -12,59 +12,9 @@
     @yield('additionalHeaders')
 </head>
 <body>
-<div class="container">
-    <nav role="navigation" class="navbar navbar-default navbar-fixed-top">
-        <!-- Responsive Navbar -->
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#collapse-list">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="/">IT Asset Management System</a>
-        </div>
-        <div class="collapse navbar-collapse" id="collapse-list">
-            <!-- Navbar contents -->
-            <ul class="nav navbar-nav">
-                <li><a href="/admin">Dashboard</a></li>
-                <li><a href="/admin/asset">Assets</a></li>
-                <li class="dropdown">
-                    <a data-toggle="dropdown" role="button">Other Managements<span class="caret"></span></a>
-                    <ul class="dropdown-menu" role="menu">
-                        <li><a href="/admin/department">Departments</a></li>
-                        <li><a href="/admin/location">Locations</a></li>
-                        <li><a href="/admin/model">Models</a></li>
-                        <li><a href="/admin/order">Orders</a></li>
-                        <li><a href="/admin/vendor">Vendors</a></li>
-                        <!--<li class="divider"></li>-->
-                    </ul>
-                </li>
-                <li class="dropdown">
-                    <a data-toggle="dropdown" role="button">Logs<span class="caret"></span></a>
-                    <ul class="dropdown-menu" role="menu">
-                        <li><a href="/admin/audit">Audit Log</a></li>
-                        <!--<li class="divider"></li>-->
-                    </ul>
-                </li>
-            </ul>
-            <ul class="nav navbar-nav navbar-right">
-                <li class="divider"></li>
-                <!-- Authentication Links -->
-                <li><a>User: {{ (Auth::check()) ? Auth::user()->name : 'Direct Access' }}</a></li>
-                <li>
-                    <a href="{{ route('logout') }}"
-                       onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                        Logout
-                    </a>
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                        {{ csrf_field() }}
-                    </form>
-                </li>
-            </ul>
-        </div>
-    </nav>
+@include('layouts.navbar')
+<main role="main" class="container pt-3">
     @yield('contents')
-</div>
+</main>
 </body>
 </html>
