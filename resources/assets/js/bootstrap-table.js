0872b30623ef95c34554c1a8c08b8587f32df44a
Unknown <sakakilai@gmail.com>
2018-01-22 11:50:03 +0800

    [Dependencies] Add flat-json for further purpose
    This JS can allow bootstrap-table to read nested objects. This will be useful in further development.

--- resources/assets/js/bootstrap-table.js
+++ resources/assets/js/bootstrap-table.js
@@ -1,6 +1,7 @@
 // Require main bootstrap-table
 require('bootstrap-table/src/bootstrap-table');
-require('bootstrap-table/src/extensions/mobile/bootstrap-table-mobile')
+require('bootstrap-table/src/extensions/mobile/bootstrap-table-mobile');
+require('bootstrap-table/src/extensions/flat-json/bootstrap-table-flat-json');
 
 // Require language files
 require('bootstrap-table/src/locale/bootstrap-table-en-US');
